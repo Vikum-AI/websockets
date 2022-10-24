d8de79501787a9350cc4d7b9d861c99f0d39a026
Vikum-AI <63595074+Vikum-AI@users.noreply.github.com>
2022-10-24 23:54:46 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
 
 function App() {
+  const nameRef = useRef(null);
+  const msgRef = useRef(null);
+  const [data, setData] = useState("");
+
+  const onSubmit = () => {
+    connection.send("name", nameRef.current.value);
+    console.log(nameRef.current.value);
+    connection.send("msg", msgRef.current.value);
+  };
+
+  const url = "ws://localhost:8080";
+  const connection = new WebSocket(url);
+  connection.binaryType = "arraybuffer";
+
+  // connection.onopen = () => {
+  //   connection.send("Message From Client");
+  // };
+
+  connection.onerror = (error) => {
+    console.log(`Errors: ${error}`);
+  };
+
+  connection.onmessage = (e) => {
+    console.log(e.data);
+    // setData(e.data);
+  };
+
+  connection.onmessage("msg", (dataFromServer) => setData(dataFromServer));
+
+  connection.addEventListener("msg", (dataFromServer) =>
+    console.log(dataFromServer)
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="flex flex-col gap-4 m-4">
+      <div>
+        <label className="mr-2">Name</label>
+        <input
+          type="text"
+          className="border focus:ring-none focus:border"
+          ref={nameRef}
+        />
+      </div>
+      <div>
+        <label className="mr-2">Message</label>
+        <input
+          type="text"
+          className="border focus:ring-none focus:border"
+          ref={msgRef}
+        />
+      </div>
+      <div>
+        <button
+          className="bg-blue-500 p-2 rounded-md text-white text-sm font-medium"
+          onClick={onSubmit}
         >
-          Learn React
-        </a>
-      </header>
+          Send message
+        </button>
+      </div>
+      <div>{data && data}</div>
     </div>
   );
 }
